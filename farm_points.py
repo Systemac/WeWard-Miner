fce15a1bcf1f49d306e6d6d4af548d69f9e0ac3d
Alessandro Maggio <alex.tkd.alex@gmail.com>
2022-02-24 14:06:02 +0100

    Create a sample script for points farming, with schedule. Each 1hrs see the videos and each x hours validate the steps

---
+++ farm_points.py
@@ -0,0 +1,38 @@
+import json
+import os
+import sys
+import time
+import schedule
+import logging
+
+from WeWard import WeWard
+
+logging.basicConfig(
+    format="%(asctime)s - %(levelname)s - %(name)s - [%(funcName)s]: %(message)s",
+    datefmt="%d/%m %H:%M:%S",
+    level=logging.INFO,
+)
+logger = logging.getLogger(__name__)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        logger.info("Usage: farm_points.py <jsonfile.json>")
+        sys.exit(1)
+
+    fpath = sys.argv[1]
+    if fpath is not None and fpath.endswith(".json") and os.path.isfile(fpath):
+        weward = WeWard(fpath)
+
+        schedule.every().hour.do(weward.ads_reward_v2)
+
+        jobs = ["06:00", "09:00", "10:15", "13:30", "15:00", "18:30", "23:00"]
+        for job_number in range(0, len(jobs)):
+            schedule.every().day.at(jobs[job_number]).do(weward.push_and_validate_step, job_number=job_number)
+
+        while True:
+            schedule.run_pending()
+            time.sleep(1)
+
+    else:
+        logger.error(f"Please review your .json file (2th arg): {fpath}")
