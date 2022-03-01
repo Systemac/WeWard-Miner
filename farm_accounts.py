edd509c8018ded1b810116ad93b4a902f1ba0bd6
Alessandro Maggio <alex.tkd.alex@gmail.com>
2022-03-01 22:00:24 +0100

    Instantiate WeWard

--- farm_accounts.py
+++ farm_accounts.py
@@ -1,30 +1,38 @@
+import logging
 import sys
 import time
-import logging
 
 from WeWard import WeWard
 
-if __name__ == '__main__':
+logging.basicConfig(
+    format="%(asctime)s - %(levelname)s - %(name)s - [%(funcName)s]: %(message)s",
+    datefmt="%d/%m %H:%M:%S",
+    level=logging.INFO,
+)
+logger = logging.getLogger(__name__)
+
+if __name__ == "__main__":
     if len(sys.argv) != 3:
         logger.info("Usage: farm_accounts.py <email> <password>")
         sys.exit(1)
 
+    weward = WeWard()
     email = sys.argv[1].strip()
     password = sys.argv[2].strip()
 
-    WeWard.request_signin_with_email(email)
+    weward.request_signin_with_email(email)
     time.sleep(5)
 
-    url = WeWard.get_url_from_email(email, password)
+    url = weward.get_url_from_email(email, password)
     while url is None:
         time.sleep(15)
-        url = WeWard.get_url_from_email(email, password)
+        url = weward.get_url_from_email(email, password)
 
-    custom_token = WeWard.parse_token_from_url(url)
+    custom_token = weward.parse_token_from_url(url)
 
-    session_path = WeWard.signup(custom_token)
+    session_path = weward.signup(custom_token)
 
-    weward = WeWard(session_path)
+    weward.load_session(session_path)
 
     weward.save_consents()
     weward.update_step_source()
@@ -33,4 +41,4 @@ if __name__ == '__main__':
     weward.complete_sponsorship_step()
 
     profile_data = weward.get_profile()
-    print(profile_data)
+    logger.info(profile_data)
